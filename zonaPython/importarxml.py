c328f552a0c76091c12c15216794557300963d0c
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-03-06 14:09:46 -0300

    algo parte 2

--- zonaPython/importarxml.py
+++ zonaPython/importarxml.py
@@ -1,15 +1,30 @@
-from xml.dom import minidom
+from xml.etree.ElementTree import parse
+import os
+os.system('cls')
 
-doc = minidom.parse("testeos/zonaPython/ejemplo.xml")
+estructura_xml  = parse("C:/Users/Interhouse HP/Desktop/zonaTesting/testeos/zonaPython/ejemplo.xml") 
+xml = parse("C:/Users/Interhouse HP/Desktop/zonaTesting/testeos/zonaPython/archivo.xml")
 
-nombre = doc.getElementsByTagName("nombre")[0]
-print(nombre.firstChild.data)
+# Obtiene el elemento raíz:
 
-empleados = doc.getElementByTagName("empleado")
-for empleado in empleados:
-    sid = empleado.getAttribute("id")
-    username = empleado.getElementsByTagName("username")[0]
-    password = empleado.getElementsByTagName("password")[0]
-    print("id:%s " % sid)
-    print("username:%s" % username.firstChild.data)
-    print("password:%s" % password.firstChild.data)
+for item in estructura_xml.iterfind('record'):
+    print(item.findtext('first_name'))
+print("***************************")
+import pandas as pd
+first_name = []
+last_name = []
+gender = []
+ip_address = []
+for item in estructura_xml.iterfind('record'):
+    first_name.append(item.findtext('first_name'))
+    last_name.append(item.findtext('last_name'))
+    gender.append(item.findtext('gender'))
+    ip_address.append(item.findtext('ip_address'))
+    
+df = pd.DataFrame({'First name':first_name, 'Last name':last_name, 'Gender':gender, 'ip':ip_address})
+
+print(df)
+for item in xml.iterfind('Emisor'):
+    print(item)
+    print(item.findtext('CorreoEmisor'))
+
